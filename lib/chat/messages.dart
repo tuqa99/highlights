74b2a8b33386c8e9ef51a9df753bd5bff6c1906b
Amer Melhem9 <amermelhem.99@icloud.com>
2022-11-14 03:46:20 +0300

    hello chat

---
+++ lib/chat/messages.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'message_bubble.dart';
+
+class Messages extends StatelessWidget {
+  User? auth = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    final docId = auth!.uid;
+
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy(
+              'createdAt',
+              descending: true,
+            )
+            .snapshots(),
+        builder: (context, AsyncSnapshot chatSnapshot) {
+          if (chatSnapshot.connectionState == ConnectionState.waiting &&
+              !chatSnapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final chatDocs = chatSnapshot.data!.docs;
+          return ListView.builder(
+            reverse: true,
+            itemCount: chatDocs.length,
+            itemBuilder: (ctx, index) => MessageBubble(
+              chatDocs[index]['text'],
+              chatDocs[index]['username'],
+              chatDocs[index]['userId'] == docId,
+              key: ValueKey(chatDocs[index].id),
+            ),
+          );
+        });
+  }
+}
